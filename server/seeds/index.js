7267bcf8d53d923ec72559964694caa54dcc7ed4
Kayahs F <akshaykmanchanda@gmail.com>
2020-04-03 14:30:22 -0400

    Added external salt that generates on command. Added default password use from .env file to use generated salt

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,15 +1,23 @@
 import { Pool } from 'pg'
-import DE from 'squel'
+import squel from 'squel'
 import config from '../config/default.json'
-// import options from '../config/options.json'
+import options from '../config/options.json'
+import dotenv from 'dotenv'
+import bcrypt, { hash } from 'bcryptjs'
 
-const squel = DE.useFlavour('postgres')
+dotenv.config()
+
+const defaultPassword = process.env.DEFAULT_PASSWORD
+
+const squelps = squel.useFlavour('postgres')
+
+const hashedPassword = bcrypt.hashSync(defaultPassword, options.salt)
 
 const userSeeds = [
   {
     fullname: 'Akshay Manchanda',
     email: 'akshaykmanchanda@gmail.com',
-    password: '$2a$12$C3fzh/z1u9PpTxjEPsK1SefXVritcsDfcL6ftQzWSkfgouSKR6BfS'
+    password: hashedPassword
   }
 ]
 
@@ -23,7 +31,7 @@ const seed = async () => {
     await Promise.all(
       userSeeds.map(userSeed =>
         pg.query(
-          squel
+          squelps
             .insert()
             .into('portfolio.users')
             .setFields(userSeed)
