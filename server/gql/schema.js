bd06e6df2784fb82edef0c62c33495f37f6ac907
Akshay Manchanda <akshaykmanchanda@gmail.com>
2020-04-03 08:31:36 -0400

    Changes to schema for auth flow

--- server/gql/schema.js
+++ server/gql/schema.js
@@ -1,8 +1,28 @@
 import { gql } from 'apollo-server-express'
 
 export default gql`
+  union LoginResult = LoginResponse | Error
+
   type Query {
     AllUsers: [User]!
+  }
+
+  type Mutation {
+    login(input: LoginInput!): LoginResult!
+  }
+
+  type LoginResponse {
+    csrfToken: String!
+    user: User!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
+  type Error {
+    message: String!
   }
 
   type User {
