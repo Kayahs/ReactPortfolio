66af586a4b18aa853290678e86a2a13a20f3cbec
Kayahs F <akshaykmanchanda@gmail.com>
2021-03-14 23:16:05 -0400

    Fix no catch-all in Router by adding a Switch container

--- client/src/components/navigation/MainRouter.js
+++ client/src/components/navigation/MainRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import { HeaderContainer, HomePage, AboutPage, PortfolioPage, Resume } from 'components'
 import { history } from 'lib'
 
@@ -7,10 +7,13 @@ const MainRouter = () => (
   <Router history={history}>
     <div className="App">
       <HeaderContainer />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/about" exact component={AboutPage} />
-      <Route path="/resume" component={Resume} />
-      <Route path="/portfolio" exact component={PortfolioPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" exact component={AboutPage} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/portfolio" exact component={PortfolioPage} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   </Router>
 )
