ab0485018ec4b4396fe8671f9709a969f70430c8
Kayahs F <akshaykmanchanda@gmail.com>
2021-03-20 17:20:07 -0400

    Add shell for contact page

--- client/src/components/pages/ContactPage.js
+++ client/src/components/pages/ContactPage.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import * as Yup from 'yup'
+import {useFormik} from 'formik'
+import {FormContext} from 'lib'
+import {TextInput, FormContainer, SubmitButton, StyledForm} from 'components'
 
 const ContactPage = () => <div>Contact Page</div>
 
