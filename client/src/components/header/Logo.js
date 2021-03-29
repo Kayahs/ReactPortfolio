3f948ad3f790509a51e90430b4dbb9afe4228edc
Kayahs F <akshaykmanchanda@gmail.com>
2021-03-29 05:44:05 -0400

    Remove unnecessary code from Logo

--- client/src/components/header/Logo.js
+++ client/src/components/header/Logo.js
@@ -1,17 +1,5 @@
 import { SLogo, SLogoLink } from 'components'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-
-const styles = {
-  logoDiv: {
-    width: '40%',
-    display: 'flex',
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: '40px'
-  }
-}
 
 const Logo = () => (
   <SLogo>
